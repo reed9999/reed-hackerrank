e85acfe6f5c453407910f40f1e29484b10b50eae
Philip J. Reed <>
2019-01-12 11:48:24 -0800

    Passes the two-student testcase but failing on simple three-student It looks like candies_array is ending up with 2 elements, not 3.

--- candies.py
+++ candies.py
@@ -19,38 +19,54 @@ def compare_min_scores(x, y):
         return 1
 
 def adjust_candies(scores, candies):
-    xsc, ysc = scores
-    xcan, ycan = candies
+    xsc, ysc = scores[0], scores[1]
+    xcan, ycan = candies[0], candies[1]
     if xsc > ysc:
         xcan = max (xcan, ycan + 1)
     elif ysc > xsc:
         ycan = max (ycan, xcan + 1)
+    # I thought we could do operations directly on the slice...
+    candies[0], candies[1] = xcan, ycan
+    # ... but that may be my overreliance on pandas slices. :)
+    # Instead use the returned value.
     return (xcan, ycan)
 
 
-def candies(n, arr):
-    candies_arr = [0] * n
-    if n == 2:
-        x = arr[0]
-        y = arr[1]
-        adjust_candies((x, y), (candies_arr[0], candies_arr[1]))
-        return sum(candies_arr)
+def candies_impl(arr, candies_slice):
+    n = len(arr)
+    if n == 1:
+        return candies_slice[0]
+    # elif n == 2:
+    #     x = arr[0]
+    #     y = arr[1]
+    #     adjust_candies((x, y), (candies_arr[0], candies_arr[1]))
+    #     return sum(candies_arr)
     else:
-        # pick a len roughly at the midpoint. It shouldn't matter if it's exactly the midpoint.
+        # pick a len roughly at the midpoint. It shouldn't matter if it's exactly the midpoint as long as
+        # each side has at least one element.
         i = int( n / 2 )
-        left = arr[:i+1]
-        right = arr[i+1:]
-        # This is buggy -- we need to assess the point of connection between them for rules violations
-        return candies(i, left) + candies(n - i, right)
+        left = arr[:i]
+        right = arr[i:]
+        new_candy_counts = adjust_candies(arr[i-1:i+1], candies_slice[i-1:i+1])
+        # Note that we can change candies_array directly by changing values in the slice.
+        candies_slice[i-1], candies_slice[i] = new_candy_counts
+        return candies_impl(left, candies_slice[:i]) + candies_impl(right, candies_slice[i:])
 
+def candies(n, arr):
+    assert n == len(arr)
+    global candies_arr
+    candies_arr = [1] * n
+    rv = candies_impl(arr, candies_arr)
+    assert rv == sum(candies_arr)
+    return rv
 
 def harness():
+    assert 6 == candies(3, [4, 5, 6])
+    assert 3 == candies(2, [4, 5])
     assert (2, 1) == adjust_candies((5, 4), (1, 1))
     assert (17, 2) == adjust_candies((4, 4), (17, 2))
     assert (17, 18) == adjust_candies((4, 5), (17, 2))
     assert (17, 17) != adjust_candies((4, 5), (17, 2))
-    assert 9999 != candies(3, [4, 5, 6])
-    assert 3 == candies(2, [4, 5])
 if __name__ == '__main__':
     harness()
     exit()
