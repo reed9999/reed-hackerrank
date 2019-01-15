d30b5811953745fbf9f4e6477d240f857871ff27
Philip J. Reed <>
2019-01-15 13:28:38 -0800

    Refactored. Time to declare victory.

--- candies.py
+++ candies.py
@@ -12,7 +12,7 @@ import sys
 # test case 3 should return: 160929
 # test case 14 should return:        204867
 
-# The basic concept of simple_attempt is an implementation of
+# The basic concept of candies_impl is an implementation of
 # https://www.hackerrank.com/challenges/candies/forum/comments/81516
 # but I added the tie handling, especially the partitioning, as orginal work.
 # All that remains is to refactor and do other cleanup.
@@ -21,58 +21,45 @@ import sys
 PARTITION_COUNT = 5
 
 def find_partitions(lscores,):
-    #Inefficient to find all but return only the first ones. Still better than crazy recursion.
+    #Inefficient to find all but return only the first ones. But better than crazy amounts of recursion.
     return [i for i in range(1, len(lscores)) if lscores[i] == lscores[i-1]][:PARTITION_COUNT]
 
-
-# This has grown grotesque and needs to be refactored but I'm striking while the iron is hot in
-# stream of consciousness.
-
-def simple_attempt(lscores, possible_ties=True):
-    total_score = 0
-    if possible_ties:
-        # First find the next N ties.
-        partitions = find_partitions(lscores)
-        partition_count = len(partitions)
-        if partition_count == 0:
-            return simple_attempt(lscores, False)
-        last = 0
-        for i in range(partition_count-1):
-            # kick off recursion for the first N-1 which should be clean now.
-            index = partitions[i] #The rightmost of the two tied students
-            total_score += simple_attempt(lscores[last:index], False)
-            last = index
-        index = partitions[partition_count-1]
-        total_score += simple_attempt(lscores[last:index], True)
-
-    n = len(lscores)
-    lcandies = [1] * n      #moved here the calling code.
+def result_for_segment_without_ties(segment_scores, ):
+    n = len(segment_scores)
+    lcandies = [1] * n
     for i in range(1, n):
-        if lscores[i] > lscores [i-1]:
+        if segment_scores[i] > segment_scores [i-1]:
             lcandies[i] = lcandies[i-1] + 1
 
-            # No longer need to deal with ties here.
-        # if lscores[i] == lscores[i - 1]:
-        #     # ties are interesting. Since it doesn't matter how the two compare,
-        #     # we can split the whole thing into a new right-hand piece and recurse.
-        #     n = i
-        #     right_partial_answer = simple_attempt(lscores[i:])
-        #     lscores = lscores[:n]
-        #     lcandies = lcandies[:n]
-        #     break
-
     for i in range(n-2, -1, -1):
-        if lscores[i] > lscores [i+1] and lcandies[i] <= lcandies[i+1]:
-            # This time it's important not to change it if it's already correct.
+        if segment_scores[i] > segment_scores [i+1] and lcandies[i] <= lcandies[i+1]:
+            # On the second pass it's important not to change it if it's already correct.
             lcandies[i] = lcandies[i+1] + 1
     return sum(lcandies)
+
+def candies_impl(lscores, ):
+    total_score = 0
+    # First find the next N ties.
+    partitions = find_partitions(lscores)
+    partition_count = len(partitions)
+    if partition_count == 0:
+        return result_for_segment_without_ties(lscores,)
+    last = 0
+    for i in range(partition_count-0):
+        # Call the calculator for the first N-1 which should be clean now.
+        index = partitions[i] #The rightmost of the two tied students
+        total_score += result_for_segment_without_ties(lscores[last:index],)
+        last = index
+    # Now handle the last one of the partition recursively
+    total_score += candies_impl(lscores[last:],)
+    return total_score
 
 def candies(n, arr):
     assert n == len(arr)
     lcandies = [1] * n
     # rv = candies_impl(arr, lcandies, [0, n])
     # rv = brute_force_approach(arr, lcandies,)
-    rv = simple_attempt(arr, True)
+    rv = candies_impl(arr,)
     return rv
 
 def harness():
@@ -88,7 +75,9 @@ def harness():
     # assert (17, 18) == adjust_candies([4, 5,], [17, 2,])
     # assert (17, 17) != adjust_candies([4, 5,], [17, 2,])
 if __name__ == '__main__':
-    # harness();    exit()
+    harness()
+    print("Harness OK")
+    # exit()
     os.environ['OUTPUT_PATH'] = 'temp.txt'
     # fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
