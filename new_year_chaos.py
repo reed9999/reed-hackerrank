402565807be4a574746cca64073ced54ff759a4b
Philip J. Reed <>
2019-01-24 16:47:38 -0800

    Explanation of recursive algorithm; not in code yet.

--- new_year_chaos.py
+++ new_year_chaos.py
@@ -7,14 +7,23 @@ import random
 import re
 import sys
 
+"""Key test case
+8
+1 2 5 3 7 8 6 4
+Proceed to the 5 in place 3. That's two bribes so count 2. Normalize the remaining array:
+3 7 8 6 4
+becomes
+1 4 5 3 2
+Proceed to 4. Count 2 (4 cumulative) leaving 5 3 2 -> 3 2 1.
+Count 2 for the 3 (6 cumul) leaving 2 1 which of course is 1 more.
+"""
 # It's saying both testcases
 # are too chaotic.
 def minimumBribes(q):
-    is_chaotic = any([(original - ix > 2) for ix, original in enumerate(q)])
-    rv = sum([abs(ix - original) for ix, original in enumerate(q)])
+    is_chaotic = any([(original - ix - 1 > 2 ) for ix, original in enumerate(q)])
+    rv = sum([(original - ix - 1) for ix, original in enumerate(q) if original > ix + 1])
     if (is_chaotic):
         print ("Too chaotic")
-        print ([(original - ix > 2) for ix, original in enumerate(q)])
         return "CHAOS"
     else:
         print(rv)
