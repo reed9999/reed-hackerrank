e39ad7dc6aef0bcc94d9e2c18ed62cd138a95c94
Philip J. Reed <>
2019-01-24 22:13:21 -0800

    new_year_chaos now has a working algo but times out in long TCs.

--- new_year_chaos.py
+++ new_year_chaos.py
@@ -17,19 +17,54 @@ becomes
 Proceed to 4. Count 2 (4 cumulative) leaving 5 3 2 -> 3 2 1.
 Count 2 for the 3 (6 cumul) leaving 2 1 which of course is 1 more.
 """
-# It's saying both testcases
-# are too chaotic.
+
+# TC 7 times out. Expected value is:
+# 115173
+# Too chaotic
+# 115013
+# Too chaotic
+
 def minimumBribes(q):
-    is_chaotic = any([(original - ix - 1 > 2 ) for ix, original in enumerate(q)])
-    rv = sum([(original - ix - 1) for ix, original in enumerate(q) if original > ix + 1])
+    if test_for_chaos(q):
+        print("Too chaotic")
+        return "CHAOS!"
+     # This might be easier to follow with a list comprehension and/or reduce but it's still pretty
+    # straightforward.
+    rv = minimumBribes_impl(q, 0)
+    print(rv)
+    return rv
+
+def minimumBribes_impl(q, subtotal):
+    # iterator = enumerate(q)
+    # for ix, item in enumerate(q):
+    #     print(ix)
+    #     print(item)
+    for ix, original in enumerate(q):
+        ix += 1     #one-based, not zero-based
+        if ix == original:
+            continue
+        # Eventually rather than a separate test_for_chaos we could test here, probably.
+        return minimumBribes_impl(normalize(q[ix:]), subtotal + (original - ix))
+    return subtotal
+
+def normalize(q):
+    rv = []
+    sorted_q = sorted(q)
+    # Possible list comprehension
+    for item in q:
+        rv.append(sorted_q.index(item)+1)
+    return rv
+
+def test_for_chaos(q):
+    is_chaotic = any([(original - ix - 1 > 2) for ix, original in enumerate(q)])
     if (is_chaotic):
-        print ("Too chaotic")
-        return "CHAOS"
-    else:
-        print(rv)
-        return rv
+        return True
 
 if __name__ == '__main__':
+    arr = [1, 2, 5, 3, 7, 8, 6, 4,]
+    minimumBribes(arr)
+    exit()
+
     t = int(input())
 
     for t_itr in range(t):
