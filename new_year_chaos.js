2bc9442796d1de29a6cc6ba7b8a63f92acf0974d
Philip J. Reed <>
2019-01-26 15:29:34 -0800

    PERF ISSUES. JS port of Python code passes all except the long tests On the long tests it gets performance related timeouts.

--- new_year_chaos.js
+++ new_year_chaos.js
@@ -1,5 +1,9 @@
 // See https://www.hackerrank.com/challenges/new-year-chaos/problem
-// I want to port my Python implementation to JS
+// Successfully (sorta) ported my Python implementation to JS, but like Python it still fails
+// some long tests that time out. Since it's doing a lot of unneccessary sorts, that's
+// probably not surprising.
+
+
 
 'use strict';
 
@@ -35,28 +39,6 @@ function minimumBribes(q) {
     return rv;
 }
 
-// def minimumBribes(q):
-//     if test_for_chaos(q):
-//         print("Too chaotic")
-//         return "CHAOS!"
-//      # This might be easier to follow with a list comprehension and/or reduce but it's still pretty
-//     # straightforward.
-//     rv = minimumBribes_impl(q, 0)
-//     print(rv)
-//     return rv
-//
-// def minimumBribes_impl(q, subtotal):
-//     # iterator = enumerate(q)
-//     # for ix, item in enumerate(q):
-//     #     print(ix)
-//     #     print(item)
-//     for ix, original in enumerate(q):
-//         ix += 1     #one-based, not zero-based
-//         if ix == original:
-//             continue
-//         # Eventually rather than a separate test_for_chaos we could test here, probably.
-//         return minimumBribes_impl(normalize(q[ix:]), subtotal + (original - ix))
-//     return subtotal
 
 function minimumBribes_impl(q, subtotal) {
     for (var i=1; i <= q.length; i++) {
@@ -73,25 +55,12 @@ function minimumBribes_impl(q, subtotal) {
 function normalize(q,) {
     var rv = [];
     var sorted_q = q.slice(0).sort(function(a, b) {return a - b;})
-    for (var item in q) {
+    for (var item of q) {
         rv.push(sorted_q.indexOf(item)+1);
     }
     return rv;
 }
 
-//
-// def normalize(q):
-//     rv = []
-//     sorted_q = sorted(q)
-//     # Possible list comprehension
-//     for item in q:
-//         rv.append(sorted_q.index(item)+1)
-//     return rv
-//
-// def test_for_chaos(q):
-//     is_chaotic = any([(original - ix - 1 > 2) for ix, original in enumerate(q)])
-//     if (is_chaotic):
-//         return True
 
 function testForChaos(q) {
     for (var i=1; i <= q.length; i++) {
@@ -104,6 +73,11 @@ function testForChaos(q) {
 
 
 function main() {
+    console.log("hello")
+    minimumBribes([1, 2, 5, 3, 7, 8, 6, 4, ]);
+}
+
+function standardMain() {
     const t = parseInt(readLine(), 10);
 
     for (let tItr = 0; tItr < t; tItr++) {
@@ -115,34 +89,11 @@ function main() {
     }
 }
 
-// """Key test case
-// 8
-// 1 2 5 3 7 8 6 4
-// Proceed to the 5 in place 3. That's two bribes so count 2. Normalize the remaining array:
-// 3 7 8 6 4
-// becomes
-// 1 4 5 3 2
-// Proceed to 4. Count 2 (4 cumulative) leaving 5 3 2 -> 3 2 1.
-// Count 2 for the 3 (6 cumul) leaving 2 1 which of course is 1 more.
-// """
-//
+
+main();
+
 // # TC 7 times out. Expected value is:
 // # 115173
 // # Too chaotic
 // # 115013
 // # Too chaotic
-//
-//
-// if __name__ == '__main__':
-//     arr = [1, 2, 5, 3, 7, 8, 6, 4,]
-//     minimumBribes(arr)
-//     exit()
-//
-//     t = int(input())
-//
-//     for t_itr in range(t):
-//         n = int(input())
-//
-//         q = list(map(int, input().rstrip().split()))
-//
-//         minimumBribes(q)
